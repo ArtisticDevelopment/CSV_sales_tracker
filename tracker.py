a29fb270d03963ff0a7b6a979c6d7f6ae7c8e198
Silas Green <scgreen160@gmail.com>
2025-05-12 12:57:42 -0400

    initial commit

---
+++ tracker.py
@@ -0,0 +1,15 @@
+#TODO: 
+# 1 Import necessary libraries (csv, datetime)
+# 2 Define the product_data dictionary with product IDs, names, and unit prices
+# 3 Read the "product_sales.txt" file and store the product IDs in a list
+# 4 Loop through the list of product ID's
+# 5 Appdn lists containing the data points for each product ID to a "list of lists"
+   # 5.1 Create a current_date variaable outside of the loop and set it equal to today
+   # 5.2 Create a counter variable outside the loop, and set it to 1
+   # 5.3 Strip the newline chracters off the end of the product IDs, and fetch...
+   
+import csv
+import datetime
+
+product_dataa = {}
+
